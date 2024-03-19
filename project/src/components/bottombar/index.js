0c45c04c156d2032ebc984d297d8b325937ffad2
sabihDev <8203sabi.rahman@gmail.com>
2024-03-19 22:48:08 +0500

    bottom bar designed

--- project/src/components/bottombar/index.js
+++ project/src/components/bottombar/index.js
@@ -1,7 +1,21 @@
+import styles from '@/ui/bottombar.module.css';
+import Link from 'next/link';
+import { GoHomeFill } from "react-icons/go";
+import { FaRegCompass } from "react-icons/fa6";
+import { BiSolidMoviePlay } from "react-icons/bi";
+import { MdOutlineAddBox } from "react-icons/md";
+import { PiMessengerLogoBold } from "react-icons/pi";
+import { CgProfile } from "react-icons/cg";
+
 function Bottombar() {
     return (
-        <div>
-            Enter
+        <div className={styles.bottombar}>
+            <Link href="/"><GoHomeFill/></Link>
+            <Link href="/explore"><FaRegCompass /></Link>
+            <Link href="/reels"><BiSolidMoviePlay/></Link>
+            <Link href="/create"><MdOutlineAddBox/></Link>
+            <Link href="/messages"><PiMessengerLogoBold/></Link>
+            <Link href="/profile"><CgProfile/></Link>
         </div>
     );
 }
